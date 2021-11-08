fdff12b10bcf763280713053888ed16783f69f01
HOTSONHONET <hotsonhonet@gmail.com>
2021-11-08 19:56:33 +0530

    added width and height dist function

--- imageProbs/helper.py
+++ imageProbs/helper.py
@@ -21,6 +21,7 @@ import time
 import cv2
 from enum import Enum
 
+
 # For Data preparation
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import *
@@ -44,15 +45,41 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def giveHistogram(df: "data File", col_name: str, dark=False):
+def giveHistogram(df: "data File", col_name: str, bins=None, dark=False):
     """
     To create histogram plots
 
     """
-    fig = px.histogram(
-        df, x=col_name, template="plotly_dark" if dark else "ggplot2")
+    fig = px.histogram(df, x=col_name, template="plotly_dark" if dark else "ggplot2",
+                       nbins=bins if bins != None else 1 + int(np.log2(len(df))))
     fig.update_layout(
         title_text=f"Distribution of {col_name}",
         title_x=0.5,
     )
     fig.show()
+
+
+def widthAndHeightDist(df: "data_file", col_name: "col name that contains the img path", dark=False):
+    """
+    Give Histogram distribution of image width and height
+
+    """
+    widths = []
+    heights = []
+    bins = 1 + int(np.log2(len(df)))
+    total_images = list(df[col_name].values)
+    for idx in trange(len(total_images), desc="Collecting widths and heights...", bar_format="{l_bar}%s{bar:50}%s{r_bar}" % (Fore.CYAN, Fore.RESET), position=0, leave=True):
+        cur_path = total_images[idx]
+        h, w, _ = cv2.imread(cur_path).shape
+        widths.append(w)
+        heights.append(h)
+
+    figW = px.histogram(widths, nbins=bins,
+                        template="plotly_dark" if dark else "ggplot2")
+    figW.update_layout(title='Distribution of Image Widths', title_x=0.5)
+    figW.show()
+
+    figH = px.histogram(heights, nbins=bins,
+                        template="plotly_dark" if dark else "ggplot2")
+    figH.update_layout(title='Distribution of Image Heights', title_x=0.5)
+    figH.show()
