2e9fddc99d1e4a4889d169782b957351cef074bc
HOTSONHONET <hotsonhonet@gmail.com>
2021-11-08 18:58:43 +0530

    Added Histogram Builder function

--- imageProbs/helper.py
+++ imageProbs/helper.py
@@ -1,5 +1,52 @@
-import numpy as np
-import tensorflow as tf
-import pandas as pd
-import cv2
+"""
+Importing Libraries
 
+"""
+
+
+# Standard imports
+import os
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import trange
+from colorama import Fore
+from glob import glob
+import json
+from pprint import pprint
+import time
+import cv2
+from enum import Enum
+
+# For Data preparation
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import *
+from sklearn.metrics import *
+
+# For building models
+import torch
+from torch.utils.data import DataLoader, Dataset
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def giveHistogram(df: "data File", col_name: str, dark=False):
+    """
+    To create histogram plots
+
+    """
+    fig = px.histogram(
+        df, x=col_name, template="plotly_dark" if dark else "ggplot2")
+    fig.update_layout(
+        title_text=f"Distribution of {col_name}",
+        title_x=0.5,
+    )
+    fig.show()
