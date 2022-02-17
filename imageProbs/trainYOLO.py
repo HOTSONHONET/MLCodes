8b590b3cde121da1d059dbec50818590b7307fcb
HOTSONHONET <56304060+HOTSONHONET@users.noreply.github.com>
2022-02-17 19:40:53 +0530

    added make_subplots module

--- imageProbs/trainYOLO.py
+++ imageProbs/trainYOLO.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
+from skimage import io
 import ast
 
 import seaborn as sns
